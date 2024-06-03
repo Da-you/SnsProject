c6a4659d387667ce1b1316da84353479e4e27d93
JoHyunwoo <jhw3874@gmail.com>
2024-06-03 16:51:55 +0900

    first commit

---
+++ sns-today/src/main/java/com/prj/sns_today/SnsTodayApplication.java
@@ -0,0 +1,13 @@
+package com.prj.sns_today;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsTodayApplication.class, args);
+	}
+
+}
---
+++ sns-today/src/test/java/com/prj/sns_today/SnsTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj.sns_today;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsTodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
