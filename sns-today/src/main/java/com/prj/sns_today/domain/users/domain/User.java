359ff31b8e00cb45cc5448162bed1180bf7542ce
JoHyunwoo <jhw3874@gmail.com>
2024-06-10 20:53:05 +0900

    build.gradle > mysql dependencies 수정 스프링부트 버전 2.6.x에서는 implementation 'mysql:mysql-connector-java'으로 의존선 부여해야햠

--- sns-today/src/main/java/com/prj/sns_today/domain/users/domain/User.java
+++ sns-today/src/main/java/com/prj/sns_today/domain/users/domain/User.java
@@ -1,13 +1,13 @@
 package com.prj.sns_today.domain.users.domain;
 
 import com.prj.sns_today.domain.users.model.Role;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
